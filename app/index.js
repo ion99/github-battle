71b6532aca014f3746e9d370c2ff6d8ec46b2a5d
Ion Gorincioi <gorincioii@yahoo.com>
2016-09-01 22:07:51 -0700

    Created classes, render github avatar

--- app/index.js
+++ app/index.js
@@ -1,15 +1,51 @@
+var USER_DATA = {
+  name: "Ion Gorincioi",
+  username: "ion99",
+  image: "https://avatars3.githubusercontent.com/u/14967432?v=3&s=460"
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
+  render: function(){
+    return <img src = {this.props.imageUrl} style = {{ height: 100, width: 100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
   render: function(){
     return (
-      <div> React JS program </div>
+      <div>
+        <a href = {"https://github.com/" + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function(){
+    return (
+      <div> {this.props.name} </div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function(){
+    return (
+      <div> 
+        <ProfilePic imageUrl = {this.props.user.image} /> 
+        <ProfileName name = {this.props.user.name} /> 
+        <ProfileLink username = {this.props.user.username} /> 
+      </div>
     )
   }
 });
 
 ReactDOM.render(
-  <HelloWorld />,
+  <Avatar user = {USER_DATA}/>,
   document.getElementById('app')
 );

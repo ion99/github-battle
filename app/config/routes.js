33b766ca879a99b3cc60ed9d9ddd9811b0acb801
Ion Gorincioi <gorincioii@yahoo.com>
2016-09-17 16:43:56 -0700

    Home is a child of Main and it's rendered always if other routes aren't active

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,14 +4,19 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
+var hashHistory = ReactRouter.hashHistory;
 
 var Main = require('../components/Main');
 var Home = require('../components/Home');
 
 var routes = (
-  <Router>
+  <Router history={hashHistory}>
     <Route path = '/' component = {Main}>
-      <Route path = '/home' component = {Home} />
+      <IndexRoute component = {Home} />
+      <Route path = 'playerOne' component = {PromptContainer} />
+      <Route path = 'playerTwo' component = {PromptContainer} />
+      <Route path = 'battle' component = {ConfirmBattleContainer} />
+      <Route path = 'results' component = {ResultsContainer} />
     </Route>
   </Router>
 );

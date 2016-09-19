8419a255df05b99efd383556f1aa48e7c7d230ca
Ion Gorincioi <gorincioii@yahoo.com>
2016-09-18 20:05:55 -0700

    Built the PromptContainer UI

--- app/index.js
+++ app/index.js
@@ -6,7 +6,16 @@
 
 var React = require('react');
 var ReactDOM = require('react-dom');
-var routes = require("./config/routes.js")
+var routes = require("./config/routes.js");
+var Raven = require("raven-js");
+
+var sentryKey = '603a58c9cc874c7db5e0f9a21c158d45';
+var sentryApp = '99717';
+var sentryURL = 'https://' + sentryKey + '@sentry.io/' + sentryApp;
+
+Raven.config(sentryURL).install();
+
+console.log(window.thing.doesNotExist);
 
 // var ProfilePic = React.createClass({
 //   render: function(){

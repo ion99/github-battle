310ed439b170edd92e537c7597aa3aaaf9bed0ff
Ion Gorincioi <gorincioii@yahoo.com>
2016-09-19 17:27:44 -0700

    Built a in prompt.js a functional stateless component

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,21 +1,21 @@
 var React = require('react');
-var transparentBg = require('../styles').transparentBg;
+var Prompt = require('../components/Prompt');
 
 var PromptContainer = React.createClass({
   contextTypes: {
-    router: React.PropTypes.object.isRequired;
+    router: React.PropTypes.object.isRequired
   }
   getInitialState: function(){
     return {
-      username: "";
+      username: ""
     }
   },
-  onUpdateUser: function(e){
+  handleUpdateUser: function(e){
     this.setState({
       username: e.target.value;
     })
   },
-  onSubmitUser: function(e){
+  handleSubmitUser: function(e){
     e.preventDefault();
     var username = this.state.username;
     this.setState({
@@ -40,28 +40,11 @@ var PromptContainer = React.createClass({
   render: function(){
     // console.log(this);
     return (
-      <div className="jambotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
-        <h1>{this.props.route.header}</h1>
-        <div className="col-sm-12">
-          <form onSubmit={this.onSubmitUser}>
-            <div className="form-group">
-              <input
-                className="form-control"
-                placeholder="Github Username"
-                onChange={this.onUpdateUser}
-                value={this.state.username}
-                type="text" />
-            </div>
-            <div className="form-group col-sm-4 col-sm-offset-4">
-              <button
-                className="btn btn-block btn-success"
-                type="submit">
-                  Continue
-              </button>    
-            </div>
-          </form>
-        </div>
-      </div>
+      <Prompt 
+        onSubmitUser={this.handleSubmitUser}
+        onUpdateUser={this.handleUpdateUser}
+        header={this.props.route.header}
+        username={this.state.username} />
     )
   }
 })
